8dd716c5f9072bff1a15be24b1917e3fb4c1a667
geoenterprise <jovannyrey@hotmail.com>
2025-09-21 21:13:00 -0500

    Initial project creation and CRUD pages

---
+++ TakeawayTitans/TakeawayTitans/Data/TakeawayTitansContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RestaurantApi.Models;
+
+namespace TakeawayTitans.Data
+{
+    public class TakeawayTitansContext : DbContext
+    {
+        public TakeawayTitansContext (DbContextOptions<TakeawayTitansContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RestaurantApi.Models.MenuItem> MenuItem { get; set; } = default!;
+    }
+}
---
+++ TakeawayTitans/TakeawayTitans/MenuItem.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantApi.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
